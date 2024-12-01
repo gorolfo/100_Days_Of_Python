452a0c34e437bbf0a01c75f6790d0eab55570e1b
gorolfo <orolfoglenn@hotmail.com>
2024-11-30 20:00:05 -0500

    Updated the main.py file and renamed tipcalculator

--- bandname.py
+++ bandname.py
@@ -1,5 +1,6 @@
 #### DAY-1 CHALLENGE BEGIN #####
-"""1. Create a greeting for your program.
+"""
+1. Create a greeting for your program.
 2. Ask the user for the city that they grew up in.
 3. Ask the user for the name of a pet.
 4. Combine the name of their city and pet and show them their band name.
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 #Using import statement to get access to code from another module by importing the file/function 
-import bandname  #Working with variables to manage data
 
+should_continue = False
+
+
+while not should_continue:
+    user_choice = input("Please choose which code to run. Type between [1 to 2]: ")
+
+    match user_choice:
+        case "1":
+            import bandname  #Working with variables to manage data
+        case "2":
+            import tipcalculator #Working with data types
+
+    continue_selection = input("Would you like to try other options? Please type 'yes' or 'no': ").lower()
+
+    if continue_selection == "no":
+        should_continue = True
