0f6457c2ff893e52ff6fbc3ca981d0564f332da2
Glenn Orolfo <80312116+gorolfo@users.noreply.github.com>
2024-11-30 19:09:04 -0500

    Add files via upload
    new files added

---
+++ bandname.py
@@ -0,0 +1,14 @@
+#### DAY-1 CHALLENGE BEGIN #####
+"""1. Create a greeting for your program.
+2. Ask the user for the city that they grew up in.
+3. Ask the user for the name of a pet.
+4. Combine the name of their city and pet and show them their band name.
+5. Make sure the input cursor shows on a new line:
+"""
+
+# Solution-1
+print("Welcome to the Home of the Band")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
+##### DAY-1 CHALLENGE END #####
---
+++ main.py
@@ -0,0 +1,3 @@
+#Using import statement to get access to code from another module by importing the file/function 
+import bandname  #Working with variables to manage data
+
